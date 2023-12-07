9d49a79ff5d20783c105be00050889a02996442d
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-07 20:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const extendHex = (shortHex) => {
  if(shortHex==4){
-   return `#${shortHex[1]}${shortHex[1]}${shortHex[2]${shortHex[2]}${shortHex[3]}${shortHex[3]}}`
+   return `#${shortHex[1]}${shortHex[1]}${shortHex[2]${shortHex[2]}${shortHex[3]}${shortHex[3]}`
 }
 else
-return `#${shortHex[0]}${shortHex[0]}${shortHex[1]${shortHex[1]}${shortHex[2]}${shortHex[2]}}`
+return `#${shortHex[0]}${shortHex[0]}${shortHex[1]${shortHex[1]}${shortHex[2]}${shortHex[2]}`
 };
 
 // Do not change the code below.
